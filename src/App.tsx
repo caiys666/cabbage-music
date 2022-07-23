91fbdfddd5862df99de9bec713cb3f543c81277e
caiyongsh <caiyongsh@yonyou.com>
2022-07-23 22:15:00 +0800

    feat: 引入antd组件并且配置修改antd默认的样式文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "antd";
 
 function App() {
-  return <div className="App">cabbage</div>;
+  return (
+    <div className="App">
+      <Button type="primary">cabbage</Button>
+    </div>
+  );
 }
 
 export default App;
