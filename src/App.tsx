0c2023fdd140e40c5a11f4772b8c88bca5be9a9c
caiyongsh <caiyongsh@yonyou.com>
2022-07-24 12:38:13 +0800

    feat: 引入emotion css-in-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Button } from "antd";
+import styled from "@emotion/styled";
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">cabbage</Button>
+      <CabbageButton type="primary">cabbage</CabbageButton>
     </div>
   );
 }
+
+const CabbageButton = styled(Button)`
+  width: 100%;
+`;
 
 export default App;
